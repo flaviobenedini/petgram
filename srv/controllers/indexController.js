e55666ba0e6f50a414b736af94c0d2edd7ee55da
Flavio Benedini <flavio.benedini@me.com>
2022-05-10 11:36:27 -0300

    feat: realizado os ultimos ajustes no controller para finalizar a base.

--- srv/controllers/indexController.js
+++ srv/controllers/indexController.js
@@ -1,5 +1,12 @@
+
+// Esse controller está sendo elaborado de acordo com as demandas. como ainda não tem nenhuma view pronta, apontei pro antigo html do feed.
+
+
 const controller = {
     index: (req, res) => res.sendFile('/Users/flaviobenedini/html/digitalHouse/petgram/srv/views/feed.html'),
+    
+
+
 }
 
 module.exports = controller
