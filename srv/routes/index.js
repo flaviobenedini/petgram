e255afc93cb7a50cf0ba7a0e0a352bb0709ff9e6
Flavio Benedini <flavio.benedini@me.com>
2022-05-10 11:17:56 -0300

    feat: criando a base do arquivo controller

--- srv/routes/index.js
+++ srv/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const controller = require('../controller/indexController')
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/feed.html')));
 
 module.exports = router;
