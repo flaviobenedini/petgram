fc89722564d89ed6b279daea9295477a68edafa4
Mônica Silva <monica.flor.silva@gmail.com>
2022-05-10 22:35:25 -0300

    feat: ajustando fieldset e incluindo button

--- srv/controllers/minhaContaController.js
+++
@@ -1,9 +0,0 @@
-
-// Esse controller está sendo elaborado de acordo com as demandas. como ainda não tem nenhuma view pronta, apontei pro antigo html do feed.
-
-
-const controller = {
-    minhaConta: (req, res) => res.render('minhaconta')
-}
-
-module.exports = controller
--- srv/routes/minhaconta.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const minhaContaController = require('../controllers/minhaContaController')
-
-router.get('/', minhaContaController.minhaConta);
-
-module.exports = router;
