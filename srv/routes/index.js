e9dbeeea1d44b91ced9991086ac3d1e9f726f1da
Mônica Silva <monica.flor.silva@gmail.com>
2022-05-10 22:20:10 -0300

    feat: ajuste no router e app.js

--- srv/routes/index.js
+++ srv/routes/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 const controller = require('../controllers/indexController')
 
 router.get('/', controller.index);
+router.get('/minhaconta', controller.minhaConta);
 
 module.exports = router;
