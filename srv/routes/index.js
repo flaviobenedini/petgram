11349a1b06b2f8353df0a434c30a4fc64ed58b2b
Flavio Benedini <flavio.benedini@me.com>
2022-05-11 09:57:48 -0300

    feat: inclusão da rota termos-de-uso

--- srv/routes/index.js
+++ srv/routes/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 const controller = require('../controllers/indexController')
 
 router.get('/', controller.index);
+router.get('/termos-de-uso', controller.termos);
 
 module.exports = router;
