2b2cdf246759bf6ddf09c64922d6b0b80dfcd7e1
Flavio Benedini <flavio.benedini@me.com>
2022-05-12 12:49:26 -0300

    feat: adicionando rota para create-account

--- srv/routes/index.js
+++ srv/routes/index.js
@@ -4,12 +4,9 @@ var router = express.Router();
 const controller = require('../controllers/indexController')
 
 router.get('/', controller.index);
-<<<<<<< HEAD
 router.get('/termos-de-uso', controller.termos);
 router.get('/login', controller.login);
-
-=======
+router.get('/create-account', controller.createAccount);
 router.get('/minhaconta', controller.minhaConta);
->>>>>>> feature/feature/007-minha-conta-front
 
 module.exports = router;
