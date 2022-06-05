8498b1377256bbd04e7c2dc03908db5fcd3d5ae2
Marcos Andrade <andrademarcosferreira3@gmail.com>
2022-06-05 08:12:36 -0300

    feat: alteração do arquivo index.js considerando as rotas do backend

--- srv/routes/index.js
+++ srv/routes/index.js
@@ -5,10 +5,19 @@ const controller = require('../controllers/indexController')
 
 router.get('/', controller.index);
 router.get('/termos-de-uso', controller.termos);
-router.get('/login', controller.login);
-router.get('/create-account', controller.createAccount);
-router.get('/minhaconta', controller.minhaConta);
 
-router.get('/criar-post',controller.criarPost)
+router.get('/login', controller.login);
+router.post('/login', controller.loginSucesso);
+
+router.get('/create-account', controller.createAccount);
+router.post('/create-account', controller.createAccountSucesso);
+
+router.get('/criar-post',controller.criarPost);
+router.post('/criar-post',controller.criarPostSucesso);
+
+router.get('/minhaconta', controller.minhaConta);
+router.put('/minhaconta', controller.minhaContaSucesso);
+
+
 
 module.exports = router;
