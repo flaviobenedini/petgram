5e0c8f3d2d922dd4ce3f28a1e8d10605031c89c3
Marcos Andrade <andrademarcosferreira3@gmail.com>
2022-06-05 09:14:42 -0300

    feat: alteração do arquivo admin.js considerando as rotas do backend

--- srv/controllers/adminController.js
+++ srv/controllers/adminController.js
@@ -3,8 +3,22 @@
 const controller = {}
 
 controller.index = (req, res) => res.render('admin')
+
 controller.postagens = (req, res) => res.render('lista-postagens')
+controller.postagemShow = (req, res) => res.send('mostrar postagem')
+controller.postagemEdit = (req, res) => res.send('postagem editada')
+controller.postagemDelete = (req, res) => res.send('postagem deletada')
+
+
 controller.usuarios = (req, res) => res.render('lista-usuarios')
+controller.usuarioShow = (req, res) => res.send('mostrar usuário')
+controller.usuarioEdit = (req, res) => res.send('usuário editado')
+controller.usuarioDelete = (req, res) => res.send('usuário deletado')
+
+
 controller.comentarios = (req, res) => res.render('lista-comentarios')
+controller.comentarioShow = (req, res) => res.send('mostrar comentário')
+controller.comentarioEdit = (req, res) => res.send('comentário editado')
+controller.comentarioDelete = (req, res) => res.send('comentário deletado')
 
 module.exports = controller
--- srv/routes/admin.js
+++ srv/routes/admin.js
@@ -4,8 +4,20 @@ var router = express.Router();
 const controller = require('../controllers/adminController')
 
 router.get('/', controller.index);
-router.get('/lista-postagens', controller.postagens);
-router.get('/lista-usuarios', controller.usuarios);
-router.get('/lista-comentarios', controller.comentarios);
+
+router.get('/postagens', controller.postagens);
+router.get('/postagens/:id', controller.postagemShow);
+router.put('/postagens/:id', controller.postagemEdit);
+router.delete('/postagens/:id', controller.postagemDelete);
+
+router.get('/usuarios', controller.usuarios);
+router.get('/usuarios/:id', controller.usuarioShow);
+router.put('/usuarios/:id', controller.usuarioEdit);
+router.delete('/usuarios/:id', controller.usuarioDelete);
+
+router.get('/comentarios', controller.comentarios);
+router.get('/comentarios/:id', controller.comentarioShow);
+router.put('/comentarios/:id', controller.comentarioEdit);
+router.delete('/comentarios/:id', controller.comentarioDelete);
 
 module.exports = router;
