af16c73bece5569fed53a111536eb59796c60174
Marcos Andrade <andrademarcosferreira3@gmail.com>
2022-06-05 08:20:19 -0300

    feat: alteração do arquivo indexController.js devolvendo informação das rotas

--- srv/controllers/indexController.js
+++ srv/controllers/indexController.js
@@ -1,13 +1,22 @@
 
 // Esse controller está sendo elaborado de acordo com as demandas. como ainda não tem nenhuma view pronta, apontei pro antigo html do feed.
-const controller = {
-    index: (req, res) => res.render('index'),
-    termos: (req, res) => res.render('termos-de-uso'),
-    login: (req, res) => res.render('login'),
-    createAccount: (req, res) => res.render('create-account'),
-    minhaConta: (req, res) => res.render('minhaconta'),
-    criarPost: (req, res) => res.render('criar-post')
+const controller = {}
 
-}
+    controller.index = (req, res) => res.render('index')
+    controller.termos = (req, res) => res.render('termos-de-uso')
+
+    controller.login = (req, res) => res.render('login')
+    controller.loginSucesso = (req, res) => res.send('login realizado com Sucesso')
+
+    controller.createAccount = (req, res) => res.render('create-account')
+    controller.createAccountSucesso = (req, res) => res.send('conta criada com Sucesso')
+
+    controller.criarPost = (req, res) => res.render('criar-post')
+    controller.criarPostSucesso = (req, res) => res.send('post criado com Sucesso')
+
+    controller.minhaConta = (req, res) => res.render('minhaconta')
+    controller.minhaContaSucesso = (req, res) => res.send('conta alterada com Sucesso')
+
+    
 
 module.exports = controller
